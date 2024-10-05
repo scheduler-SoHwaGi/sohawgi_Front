94a9c6d0e58e7cd45069360e9521f4a24b719659
GuNayeon77 <listoss456@gmail.com>
2024-10-06 04:26:55 +0900

    스타일 파일 따로 분류 (ts)

--- src/components/BottomNavi/BottomNavi.tsx
+++ src/components/BottomNavi/BottomNavi.tsx
@@ -1,35 +1,21 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
 import { Link } from 'react-router-dom';
-
-// 하단바 스타일 정의
-const BottomNavWrapper = styled.nav`
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  max-width: 420px; /* 모바일 뷰의 최대 너비 */
-  height: 60px;
-  background-color: #f8f8f8;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  border-top: 1px solid #ccc;
-`;
+import * as S from './BottomNavi.style.ts';
 
 const BottomNavi: React.FC = () => {
   return (
-    <BottomNavWrapper>
+    <S.BottomNavWrapper>
       <div>
         <Link to="/">
-            일정
+          일정
         </Link>
       </div>
       <div>
         <Link to="/PlusPage">
-            더보기
+          더보기
         </Link>
       </div>
-    </BottomNavWrapper>
+    </S.BottomNavWrapper>
   );
 };
 
