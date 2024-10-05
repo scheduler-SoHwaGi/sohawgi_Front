b55236aea9959bcc8f0cf3fae7ca96c2bd0f1d2f
GuNayeon77 <listoss456@gmail.com>
2024-10-06 02:10:12 +0900

    모바일 뷰 설정

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-// App.js
 import React from 'react';
-import { ThemeProvider } from 'styled-components'; // theme 사용을 위해
-import styled from 'styled-components'; // styled-components 사용
-import theme from './style/theme.ts';  // theme 사용을 위해
+import { ThemeProvider } from 'styled-components';
+import styled from 'styled-components';
+import theme from './style/theme.ts';
+import GlobalStyle from './style/globalStyles.ts'; // GlobalStyle import
 
-// StyledSpan 컴포넌트 정의
-const StyledSpan = styled.span`
-  ${({ theme }) => theme.fonts.heading_01};
-  color: ${({ theme }) => theme.colors.bg};
+// WebViewContainer 스타일 컴포넌트 정의
+const WebViewContainer = styled.div`
+  max-width: 375px; /* 모바일 뷰의 최대 너비 */
+  width: 100%; /* 부모 요소의 너비를 가득 채우도록 설정 */
+  overflow-x: hidden; /* 수평 스크롤 방지 */
+  word-wrap: break-word; /* 단어가 길어지면 자동으로 줄바꿈 */
+  word-break: break-all; /* 단어나 숫자가 길어지면 줄바꿈 */
+  white-space: normal; /* 기본적인 줄바꿈 규칙을 따름 */
+ 
 `;
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <StyledSpan>안녕</StyledSpan> {/* StyledSpan 사용 */}
-      </div>
+      <GlobalStyle /> {/* 글로벌 스타일 적용 */}
+      <WebViewContainer>
+        1234567890#1234567890#1234567890#1234567890#1234567890#1234567890#1234567890#1234567890#1234567890#1234567890#
+      </WebViewContainer>
     </ThemeProvider>
   );
 }
--- src/style/globalStyles.ts
+++ src/style/globalStyles.ts
@@ -1,75 +1,86 @@
-// src/style/globalStyles.ts
-// 다운받은 폰트 정의
-// src/style/globalStyles.ts
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/assets/fonts/Pretendard-Thin.ttf') format('truetype');
-    font-weight: 100;
-    font-style: normal;
-  }
+    @font-face {
+        font-family: 'Pretendard';
+        src: url('/assets/fonts/Pretendard-Thin.ttf') format('truetype');
+        font-weight: 100;
+        font-style: normal;
+    }
 
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/assets/fonts/Pretendard-ExtraLight.ttf') format('truetype');
-    font-weight: 200;
-    font-style: normal;
-  }
+    @font-face {
+        font-family: 'Pretendard';
+        src: url('/assets/fonts/Pretendard-ExtraLight.ttf') format('truetype');
+        font-weight: 200;
+        font-style: normal;
+    }
 
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/assets/fonts/Pretendard-Light.ttf') format('truetype');
-    font-weight: 300;
-    font-style: normal;
-  }
+    @font-face {
+        font-family: 'Pretendard';
+        src: url('/assets/fonts/Pretendard-Light.ttf') format('truetype');
+        font-weight: 300;
+        font-style: normal;
+    }
 
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/assets/fonts/Pretendard-Regular.ttf') format('truetype');
-    font-weight: 400;
-    font-style: normal;
-  }
+    @font-face {
+        font-family: 'Pretendard';
+        src: url('/assets/fonts/Pretendard-Regular.ttf') format('truetype');
+        font-weight: 400;
+        font-style: normal;
+    }
 
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/assets/fonts/Pretendard-Medium.ttf') format('truetype');
-    font-weight: 500;
-    font-style: normal;
-  }
+    @font-face {
+        font-family: 'Pretendard';
+        src: url('/assets/fonts/Pretendard-Medium.ttf') format('truetype');
+        font-weight: 500;
+        font-style: normal;
+    }
 
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/assets/fonts/Pretendard-SemiBold.ttf') format('truetype');
-    font-weight: 600;
-    font-style: normal;
-  }
+    @font-face {
+        font-family: 'Pretendard';
+        src: url('/assets/fonts/Pretendard-SemiBold.ttf') format('truetype');
+        font-weight: 600;
+        font-style: normal;
+    }
 
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/assets/fonts/Pretendard-Bold.ttf') format('truetype');
-    font-weight: 700;
-    font-style: normal;
-  }
+    @font-face {
+        font-family: 'Pretendard';
+        src: url('/assets/fonts/Pretendard-Bold.ttf') format('truetype');
+        font-weight: 700;
+        font-style: normal;
+    }
 
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/assets/fonts/Pretendard-ExtraBold.ttf') format('truetype');
-    font-weight: 800;
-    font-style: normal;
-  }
+    @font-face {
+        font-family: 'Pretendard';
+        src: url('/assets/fonts/Pretendard-ExtraBold.ttf') format('truetype');
+        font-weight: 800;
+        font-style: normal;
+    }
 
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/assets/fonts/Pretendard-Black.ttf') format('truetype');
-    font-weight: 900;
-    font-style: normal;
-  }
+    @font-face {
+        font-family: 'Pretendard';
+        src: url('/assets/fonts/Pretendard-Black.ttf') format('truetype');
+        font-weight: 900;
+        font-style: normal;
+    }
 
-  body {
-    font-family: 'Pretendard', sans-serif;
-  }
+
+    body {
+        width: 100%;
+        min-height: 100vh;
+        margin: 0;
+        padding: 0 16px;
+        display: flex;
+        justify-content: center; /* 가로 방향 정렬
+    
+        overflow-x: hidden; /* 수평 스크롤을 숨김 */
+    }
+
+    #root {
+        max-width: 375px; /* 모바일 뷰의 최대 너비를 고정 */
+        width: 100%;
+        margin: 0 auto; /* 가운데 정렬 */
+    }
 `;
 
 export default GlobalStyle;
