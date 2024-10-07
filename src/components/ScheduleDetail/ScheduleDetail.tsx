b4dcf0ab39f4584a3e365924e4e359a287396293
GuNayeon77 <listoss456@gmail.com>
2024-10-07 17:50:01 +0900

    디테일 정보 구현

--- src/components/ScheduleDetail/ScheduleDetail.tsx
+++ src/components/ScheduleDetail/ScheduleDetail.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import hamtori from '../../assets/images/hamtori.svg';
+import * as S from './ScheduleDetail.style.ts';
 
 const ScheduleDetail = () => {
     return (
-        <div>
-            <img src={hamtori}></img>
-        </div>
+        <S.Container>
+            <S.Image src={hamtori} alt="Hamtori" />
+            <S.InfoContainer>
+                <S.DateText>9월 7일 (금)</S.DateText>
+                <S.ContentText>일정입니다일정입니다</S.ContentText>
+            </S.InfoContainer>
+        </S.Container>
     );
 };
 
