b8143040acdb3f1f3eebc4dd8de3a17e5d233774
GuNayeon77 <listoss456@gmail.com>
2024-10-07 16:41:02 +0900

    textfield 위치 및 여백 수정

--- src/components/TextField/TextField.style.ts
+++ src/components/TextField/TextField.style.ts
@@ -6,18 +6,21 @@ interface InputProps {
 
 export const Form = styled.form`
   position: relative;
-  display: inline-block;
+  display: flex; /* inline-block 대신 flex 사용 */
+  width: 100%; /* 부모 요소에 따라 너비가 조정되도록 설정 */
+  max-width: 100%; /* 최대 너비를 제한하여 너무 커지지 않도록 설정 */
+  padding: 0 0; /* 좌우에 적절한 패딩 추가 */
 `;
 
 export const Input = styled.input`
-  width: 250px;
-  padding: 15px;
-  padding-right: 75px;
-  font-size: 16px;
+  width: 100%; /* 부모(Form) 요소에 맞게 너비 조정 */
+  padding: 0.8125rem 1.0625rem;
+  padding-right: 3.6rem;
+  
+  font-family: ${({ theme }) => theme.fonts.Body_03}; /* 텍스트 폰트 스타일 */
+  color: ${({ theme }) => theme.colors.Grey_06}; /* 텍스트 색상 */
   border-radius: 10px;
   outline: none;
-  justify-content: center;
-  align-items: center;
 
   /* 기본 상태 스타일 */
   border: 1px solid ${({ theme }) => theme.colors.Grey_02};
@@ -28,8 +31,14 @@ export const Input = styled.input`
     background-color: ${({ theme }) => theme.colors.Grey_03};
     border-color: ${({ theme }) => theme.colors.Grey_06};
   }
-`;
 
+  /* Placeholder 스타일 */
+  ::placeholder {
+    color: ${({ theme }) => theme.colors.Grey_04}; /* Placeholder 색상 */
+    font-family: ${({ theme }) => theme.fonts.Body_03}; /* Placeholder 폰트 */
+    font-size: 14px; /* Placeholder 폰트 크기 */
+  }
+`;
 
 export const Button = styled.button`
   position: absolute;
--- src/pages/SchedulePage.tsx
+++ src/pages/SchedulePage.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import styled from 'styled-components';
 import TextField from '../components/TextField/TextField.tsx';
 
 const SchedulePage = () => {
     return (
-        <div>
-            <TextField></TextField>
-        </div>
+        <Container>
+            <TextField />
+        </Container>
     );
 };
 
-export default SchedulePage
+// 스타일 컴포넌트
+const Container = styled.div`
+  display: flex;
+  justify-content: center; /* 가로축 가운데 정렬 */
+  padding: 0; /* 화면 양쪽에 여백을 유지 */
+  width: 100%; /* Container를 화면 전체 너비로 설정 */
+`;
 
+export default SchedulePage;
