432b54b817cbbc13685c946a991b3020e6d4965d
GuNayeon77 <listoss456@gmail.com>
2024-10-07 18:35:50 +0900

    스케줄 페이지 상단 padding 조정

--- src/pages/SchedulePage/SchedulePage.style.ts
+++ src/pages/SchedulePage/SchedulePage.style.ts
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: grid;
   grid-template-rows: auto 1fr; /* TextField와 ScheduleCard를 세로로 나열 */
-  gap: 1rem; /* 각 요소 사이의 간격 */
+  gap: 2.31rem; /* 각 요소 사이의 간격 */
   width: 100%;
-  padding: 0rem; /* 전체 컨테이너의 내부 여백 */
+  padding: 2.75rem 0rem 0rem 0rem; /* 전체 컨테이너의 내부 여백 * 상우좌하 순서임/
   
 `;
