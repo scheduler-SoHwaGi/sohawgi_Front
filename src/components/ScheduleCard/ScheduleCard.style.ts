b8f2e21081e717bef6ee0855fdad5da7c8931aca
GuNayeon77 <listoss456@gmail.com>
2024-10-07 18:21:42 +0900

    여백 색상 변경 (배경)

--- src/components/ScheduleCard/ScheduleCard.style.ts
+++ src/components/ScheduleCard/ScheduleCard.style.ts
@@ -5,16 +5,15 @@ export const WrapperContainer = styled.div`
   display: grid;
   gap: 1.5rem; /* '일정' 텍스트와 그리드 사이 간격 */
   width: 100%;
-  background-color: #f8f9fa; /* 배경 색상 추가 */
+  background-color: ${({ theme }) => theme.colors.White};; /* 배경 색상 추가 */
   padding: 1.88rem 1.69rem; /* 내부 여백 추가 */
   border-radius: 1.7rem; /* 모서리를 둥글게 */
 `;
 
 export const Title = styled.div`
-  font-size: 1.5rem; /* 폰트 크기 */
   font-family: ${({ theme }) => theme.fonts.Body_01}; /* 테마에 정의된 폰트 스타일 사용 */
   color: ${({ theme }) => theme.colors.Grey_06}; /* 테마에 정의된 색상 사용 */
-  font-weight: bold; /* 폰트 두께 설정 */
+
 `;
 
 export const GridContainer = styled.div`
