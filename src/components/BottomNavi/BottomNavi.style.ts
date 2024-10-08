7a0d63390324c7f08b8ab2e0b11f1957f4da1e03
GuNayeon77 <listoss456@gmail.com>
2024-10-09 01:57:23 +0900

    BottomSheet 디자인 작업

--- src/components/BottomNavi/BottomNavi.style.ts
+++ src/components/BottomNavi/BottomNavi.style.ts
@@ -1,16 +1,63 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-// 하단바 스타일 정의
-export const BottomNavWrapper = styled.nav`
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  max-width: 450px; /* 모바일 뷰의 최대 너비 */
-  height: 60px;
-  background-color: #f8f8f8;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none; /* Link 자체의 밑줄 제거 */
+`;
+
+
+export const BottomNavWrapper = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
-  border-top: 1px solid #ccc;
+  background-color: ${({ theme }) => theme.colors.White}; /* 배경 흰색 */
+  height: 4rem;
+  padding-bottom: 1.8rem; /* 원하는 여백 높이 */
+  border-top: 1px solid ${({ theme }) => theme.colors.Grey_03}; /* 상단에 얇은 회색선 추가 */
+  box-shadow: none; /* 검은 배경(그림자) 제거 */
+  z-index: 1000;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  padding-top: 0.3rem;
 `;
+
+export const ScheduleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const ScheduleIcon = styled.img`
+  width: 24px;
+  height: 24px;
+  margin-bottom: 4px;
+`;
+
+export const ScheduleText = styled.span<{ isActive: boolean }>`
+  font-size: ${({ theme }) => theme.fonts.Body_05};
+  color: ${({ isActive, theme }) => (isActive ? theme.colors.Grey_06 : theme.colors.Grey_02)};
+  text-decoration: none; /* 밑줄 제거 */
+`;
+
+export const MoreContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const MoreIcon = styled.img`
+  width: 24px;
+  height: 24px;
+  margin-bottom: 4px;
+`;
+
+export const MoreText = styled.span<{ isActive: boolean }>`
+  font-size: ${({ theme }) => theme.fonts.Body_05};
+  color: ${({ isActive,theme }) => (isActive ? theme.colors.Grey_06 : theme.colors.Grey_02)};
+  text-decoration: none; /* 밑줄 제거 */
+`;
+
+
