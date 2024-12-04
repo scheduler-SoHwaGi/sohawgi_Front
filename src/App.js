b374c5f1ea39ee747802015540b0f3363da12e31
@u_limit_ <yurim@ajou.ac.kr>
2024-12-05 02:23:32 +0900

    feat: 일정 컴포넌트 가운데로 수정

--- src/App.js
+++ src/App.js
@@ -8,45 +8,44 @@ import BottomNavi from './components/BottomNavi/BottomNavi.tsx';
 
 // 페이지 컴포넌트 import
 import SchedulePage from './pages/SchedulePage/SchedulePage.tsx';
-
 import PlusPage from './pages/PlusPage.tsx';
 
 // 상위 컨테이너 스타일
 const Container = styled.div`
-  max-width: 450px;
-  width: 100%;
   display: flex;
   flex-direction: column;
+    align-items: center;
   justify-content: space-between;
   min-height: 100vh;
 `;
 
 // 콘텐츠를 감싸는 래퍼에 패딩 적용
 const ContentWrapper = styled.div`
-  padding: 0 1.13rem;
-  flex: 1;
-  overflow-x: hidden;
-  word-wrap: break-word;
-  word-break: break-all;
-  white-space: normal;
+    //padding: 0 1.13rem;
+    //width: 100%;
+    //max-width: 450px;
+    //flex: 1;
+    //overflow-x: hidden;
+    word-wrap: break-word;
+    word-break: break-all;
+    white-space: normal;
+    min-width: 340px;
 `;
-
-
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Router> {/* BrowserRouter로 감싸기 */}
+      <Router>
         <Container>
           <ContentWrapper>
-            <Routes> {/* 기본 경로로 SchedulePage를 설정 */}
+            <Routes>
               <Route path="/" element={<SchedulePage />} />
               <Route path="/SchedulePage" element={<SchedulePage />} />
               <Route path="/PlusPage" element={<PlusPage />} />
             </Routes>
           </ContentWrapper>
-          <BottomNavi /> {/* 하단바를 배치 */}
+          <BottomNavi />
         </Container>
       </Router>
     </ThemeProvider>
