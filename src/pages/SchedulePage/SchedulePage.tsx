7b8095f357672f06d1839def3ca9e29c13d34eab
@u_limit_ <yurim@ajou.ac.kr>
2024-12-05 02:22:11 +0900

    fix: tsx 확장자 제거

--- src/pages/SchedulePage/SchedulePage.tsx
+++ src/pages/SchedulePage/SchedulePage.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from 'styled-components';
-import TextField from '../../components/TextField/TextField.tsx';
-import ScheduleCard from "../../components/ScheduleCard/ScheduleCard.tsx";
-import * as S from './SchedulePage.style.ts';
+import TextField from '../../components/TextField/TextField';
+import ScheduleCard from "../../components/ScheduleCard/ScheduleCard";
+import * as S from './SchedulePage.style';
 
 const SchedulePage = () => {
     return (
