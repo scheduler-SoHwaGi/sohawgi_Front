c5408e57b516ec11706879b2918ad963dcf2c310
@u_limit_ <yurim@ajou.ac.kr>
2024-12-05 00:40:49 +0900

    refactor: 사용하지 않는 import 삭제

--- src/components/BottomNavi/BottomNavi.tsx
+++ src/components/BottomNavi/BottomNavi.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
 import * as S from './BottomNavi.style.ts';
 import plus_act from '../../assets/images/BottomNavi/plus_act.svg';
 import plus_nonact from '../../assets/images/BottomNavi/plus_nonact.svg';
@@ -9,7 +9,7 @@ import schedule_nonact from '../../assets/images/BottomNavi/schedule_nonact.svg'
 
 
 
-const BottomNavi: React.FC = () => {
+const BottomNavi = () => {
   const location = useLocation();
 
   const isScheduleActive = location.pathname === '/';
@@ -20,20 +20,20 @@ const BottomNavi: React.FC = () => {
       {/* 일정 영역 */}
       <S.StyledLink to="/">
         <S.ScheduleContainer>
-          <S.ScheduleIcon 
-            src={isScheduleActive ? schedule_act : schedule_nonact} 
-            alt="일정 아이콘" 
+          <S.ScheduleIcon
+            src={isScheduleActive ? schedule_act : schedule_nonact}
+            alt="일정 아이콘"
           />
           <S.ScheduleText isActive={isScheduleActive}>일정</S.ScheduleText>
         </S.ScheduleContainer>
       </S.StyledLink>
-      
+
       {/* 더보기 영역 */}
       <S.StyledLink to="/PlusPage">
         <S.MoreContainer>
-          <S.MoreIcon 
-            src={isMoreActive ? plus_act : plus_nonact} 
-            alt="더보기 아이콘" 
+          <S.MoreIcon
+            src={isMoreActive ? plus_act : plus_nonact}
+            alt="더보기 아이콘"
           />
           <S.MoreText isActive={isMoreActive}>더보기</S.MoreText>
         </S.MoreContainer>
