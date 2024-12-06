27b6edf6ea51907b710c963723ca98130dcdf354
@u_limit_ <yurim@ajou.ac.kr>
2024-12-06 22:29:01 +0900

    feat(ScheduleDetail): 일정 리스트 조회 리스트 UI 연결

--- src/components/ScheduleDetail/ScheduleDetail.tsx
+++ src/components/ScheduleDetail/ScheduleDetail.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import hamtori from '../../assets/images/hamtori.svg';
 import * as S from './ScheduleDetail.style';
 
-const ScheduleDetail = () => {
-    return (
-        <S.Container>
-            <S.Image src={hamtori} alt="Hamtori" />
-            <S.InfoContainer>
-                <S.DateText>9월 7일 (금)</S.DateText>
-                <S.ContentText>일정입니다일정입니다</S.ContentText>
-            </S.InfoContainer>
-        </S.Container>
-    );
+type Props = {
+  month: number;
+  day: number;
+  dayOfWeek: string;
+  title: string;
+};
+
+const ScheduleDetail = ({ month, day, dayOfWeek, title }: Props) => {
+  return (
+    <S.Container>
+      <S.Image src={hamtori} alt="Hamtori" />
+      <S.InfoContainer>
+        <S.DateText>
+          {month}월 {day}일 ({dayOfWeek})
+        </S.DateText>
+        <S.ContentText>{title}</S.ContentText>
+      </S.InfoContainer>
+    </S.Container>
+  );
 };
 
 export default ScheduleDetail;
