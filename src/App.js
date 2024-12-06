2a1f428c126cafa99b0e01fb81ff865cc53f99bd
@u_limit_ <yurim@ajou.ac.kr>
2024-12-06 22:30:56 +0900

    refactor(App.js): 공백 값 통일

--- src/App.js
+++ src/App.js
@@ -8,28 +8,28 @@ import BottomNavi from './components/BottomNavi/BottomNavi.tsx';
 
 // 페이지 컴포넌트 import
 import SchedulePage from './pages/SchedulePage/SchedulePage.tsx';
-import PlusPage from './pages/PlusPage.tsx';
+import PlusPage from './pages/PlusPage/PlusPage.tsx';
 
 // 상위 컨테이너 스타일
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-    align-items: center;
+  align-items: center;
   justify-content: space-between;
   min-height: 100vh;
 `;
 
 // 콘텐츠를 감싸는 래퍼에 패딩 적용
 const ContentWrapper = styled.div`
-    //padding: 0 1.13rem;
-    //width: 100%;
-    //max-width: 450px;
-    //flex: 1;
-    //overflow-x: hidden;
-    word-wrap: break-word;
-    word-break: break-all;
-    white-space: normal;
-    min-width: 340px;
+  //padding: 0 1.13rem;
+  //width: 100%;
+  //max-width: 450px;
+  //flex: 1;
+  //overflow-x: hidden;
+  word-wrap: break-word;
+  word-break: break-all;
+  white-space: normal;
+  min-width: 340px;
 `;
 
 function App() {
