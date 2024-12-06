c2b1e32462133f0b5a042b74ad19999a94beb1ff
@u_limit_ <yurim@ajou.ac.kr>
2024-12-06 22:29:21 +0900

    feat(TextField): 한 문장 일정 등록 api 연결

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -3,6 +3,31 @@ import * as S from './TextField.style';
 
 const TextField: React.FC = () => {
   const [isFocused, setIsFocused] = useState(false);
+  const [schedule, setSchedule] = useState<string>('');
+
+  const postSchedule = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    try {
+      const response = await fetch(
+        'http://15.165.191.48:8080/api/v1/schedules',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Nickname': 'jini',
+          },
+          credentials: 'include',
+          body: JSON.stringify({ text: schedule }),
+        },
+      );
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const data = await response.json();
+      console.log('response', data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <S.Form>
@@ -11,9 +36,11 @@ const TextField: React.FC = () => {
         placeholder="일정을 입력하세요"
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
-        //isFocused={isFocused}
+        onChange={(e) => setSchedule(e.target.value)}
       />
-      <S.Button type="submit">등록</S.Button>
+      <S.Button type="submit" onClick={postSchedule}>
+        등록
+      </S.Button>
     </S.Form>
   );
 };
