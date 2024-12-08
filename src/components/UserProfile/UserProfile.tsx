6ad98e49311228e4a4cdf2fdc36c1247efca644b
@u_limit_ <yurim@ajou.ac.kr>
2024-12-08 23:35:22 +0900

    refactor: UserInfo 컴포넌트 분리

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -1,16 +1,12 @@
 import hamtori from '../../assets/images/hamtori.svg';
-import * as S from '../../pages/PlusPage/PlusPage.style';
-import appleIcon from '../../assets/images/appleIcon.png';
 import React from 'react';
+import UserInfo from './UserInfo/UserInfo';
 
 const UserProfile = () => {
   return (
     <div>
       <img src={hamtori} alt={'hamtori'} />
-      <S.UserIcon>
-        <img src={appleIcon} alt={'appleIcon'} />
-        <p>user_name</p>
-      </S.UserIcon>
+      <UserInfo />
     </div>
   );
 };
