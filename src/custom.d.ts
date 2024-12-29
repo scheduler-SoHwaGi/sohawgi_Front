87e3c8f52543caadcf2dd1f7e7dae1660ec445cf
@u_limit_ <yurim@ajou.ac.kr>
2024-12-29 16:14:59 +0900

    feat: swift와의 iOS 통신 브릿지 연결

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -6,3 +6,14 @@ declare module '*.png' {
   const content: any;
   export default content;
 }
+declare global {
+  interface Window {
+    receiveUserInfo?: (userInfo: userInfo) => void;
+  }
+}
+
+export interface UserInfo {
+  fullName?: string;
+  email?: string;
+  userIdentifier?: string;
+}
--- src/pages/PlusPage/PlusPage.tsx
+++ src/pages/PlusPage/PlusPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // import css
 import * as S from './PlusPage.style';
@@ -7,10 +7,19 @@ import * as S from './PlusPage.style';
 import UserProfile from '../../components/UserProfile/UserProfile';
 import AddPageContainer from '../../components/AddPageContainer/AddPageContainer';
 import VersionInfo from '../../components/VersionInfo/VersionInfo';
+import { UserInfo } from '../../custom';
 
 const PlusPage = () => {
   const addPageInfoTitles = ['서비스 이용약관', '개인정보 처리방침'];
   const addPageSettingTitles = ['로그아웃', '회원탈퇴'];
+  const [userInfo, setUserInfo] = useState<UserInfo>({});
+
+  useEffect(() => {
+    window.receiveUserInfo = (info: UserInfo) => {
+      setUserInfo(info);
+      console.log(userInfo);
+    };
+  }, []);
 
   return (
     <S.AddPageContainer>
