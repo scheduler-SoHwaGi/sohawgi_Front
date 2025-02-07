0c8d3ad2041df48c2bf07810fe17f21cec93911a
@u_limit_ <yurim@ajou.ac.kr>
2025-02-07 16:58:42 +0900

    chores: 주석 제거

--- src/components/BottomNavi/BottomNavi.style.ts
+++ src/components/BottomNavi/BottomNavi.style.ts
@@ -58,5 +58,5 @@ export const MoreText = styled.span<{ $isActive: boolean }>`
   font-size: ${({ theme }) => theme.fonts.Body_05};
   color: ${({ $isActive, theme }) =>
     $isActive ? theme.colors.Grey_06 : theme.colors.Grey_02};
-  text-decoration: none; /* 밑줄 제거 */
+  text-decoration: none;
 `;
