b00cb0a73518a241c13c5073b8a167707d5a74a8
GuNayeon77 <listoss456@gmail.com>
2025-03-21 02:20:21 +0900

    toastbar error 파일 생성

---
+++ src/components/ToastBar/error.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import close from '../../assets/images/close.svg';
+import warning from '../../assets/images/warning.svg';
+
+const ToastBar = () => {
+    return (
+      <div>
+        
+      </div>
+    );
+  };
+  export default ToastBar;
