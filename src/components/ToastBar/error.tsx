98fda6b1886fc96f23a64838e9cb219e822585fb
GuNayeon77 <listoss456@gmail.com>
2025-03-22 22:38:56 +0900

    error 컴포넌트 UI 구현

--- src/components/ToastBar/error.tsx
+++ src/components/ToastBar/error.tsx
@@ -4,14 +4,14 @@ import warning from '../../assets/images/warning.svg';
 
 const ToastBar = () => {
     return (
-      <div className="w-[340px] px-[15px] py-4 bg-[#343845] rounded-[10px] inline-flex flex-col justify-center items-center gap-2.5 overflow-hidden">
-      <div className="w-[310px] h-[30px] inline-flex justify-between items-center">
+      <div className="w-[340px] px-[62x] bg-Grey_06 rounded-[10px] inline-flex flex-col justify-center items-center gap-2.5 overflow-hidden">
+      <div className="w-[310px] h-[30px] my-[16px] inline-flex justify-between items-center">
         {/* 왼쪽 경고 아이콘 + 텍스트 */}
         <div className="flex justify-start items-center gap-1.5">
           <div className="w-[30px] h-[30px] relative overflow-hidden">
             <img src={warning} alt="경고 아이콘" className="w-full h-full" />
           </div>
-          <div className="w-[252px] text-white text-[15px] font-medium font-['Pretendard']">
+          <div className="w-[252px] text-White Content_01">
             에러 메세지 내용입니다
           </div>
         </div>
