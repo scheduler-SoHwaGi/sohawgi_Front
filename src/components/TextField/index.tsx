b33212fbaa9bd747d79a31b0c5407680951dd943
@u_limit_ <76727686+urimeee@users.noreply.github.com>
2025-07-23 20:06:27 +0900

    fix: changed tracking input characteristics

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -27,9 +27,9 @@ const TextField = ({ selectedDate }: TextFieldProps) => {
     postSchedule(trimmedValue);
 
     trackEvent('register_button', {
-      input: trimmedValue,
-      timestamp: new Date().toISOString(),
+      input_length: trimmedValue.length,
     })
+
     setInputValue('');
   };
 
