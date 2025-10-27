9c7c73f1846934d1d8beae7d16c90252a01657e4
@u_limit_ <76727686+urimeee@users.noreply.github.com>
2025-10-27 18:03:32 +0900

    feat : 온보딩 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import './index.css';
-import { worker } from './mocks/browser';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
